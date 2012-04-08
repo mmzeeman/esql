0383c3fb843c63b0808d90a83d50124be6430c62
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2012-04-08 23:53:00 +0200

    Small stuff

--- src/esql_app.erl
+++ src/esql_app.erl
@@ -1,5 +1,21 @@
-
-
+%% @author Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
+%% @copyright 2012 Maas-Maarten Zeeman
+%%
+%% @doc Erlang Database Connection Framework
+%%
+%% Copyright 2012 Maas-Maarten Zeeman
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
 
 -module(esql_app).
 
@@ -8,9 +24,10 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+% @doc Start esql
 start(_StartType, _StartArgs) ->
-    io:fwrite(standard_error, "Starting esql.~n", []),
     esql_pool_sup:start_link().
 
+% @doc and stop it.
 stop(_State) ->
     ok.
