65e3b680e64e998a29bf3968560c82543b2d8d7d
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2012-04-08 15:18:47 +0200

    Adding poolboy worker

---
+++ src/esql_worker.erl
@@ -0,0 +1,70 @@
+%% @author Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
+%% @copyright 2012 Maas-Maarten Zeeman
+%%
+%% @doc Erlang Database Connection Framework
+%%
+%% Copyright 2012 Maas-Maarten Zeeman
+%% 
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%% http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+
+-module(esql_worker).
+
+-behaviour(gen_server).
+-behaviour(poolboy_worker).
+
+-export([start_link/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {esql_connection}).
+
+% @doc Start the poolboy worker connection
+start_link(Opts) ->
+    gen_server:start_link(?MODULE, Opts, []).
+
+% @doc Initiate the connection.
+init(Opts) ->
+    process_flag(trap_exit, true),
+   
+    Driver = proplists:get_value(driver, Opts),
+    Args = proplists:get_value(args, Opts),
+
+    {ok, Connection} = esql:open(Driver, Args),
+    {ok, #state{esql_connection=Connection}}.
+
+% @doc ...
+handle_call({test}, _From, #state{esql_connection=Conn}=State) ->
+    {reply, ok, State};
+handle_call(Message, _From, State) ->
+    {stop, {unknown_call, Message}, State}.
+
+% @doc ..
+handle_cast(Message, State) ->
+    {stop, {unknown_cast, Message}, State}.
+
+% @doc
+handle_info(stop, State) ->
+    {stop, shutdown, State};
+handle_info({'EXIT', _, _}, State) ->
+    {stop, shutdown, State};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+% @doc Close the connection
+terminate(_Reason, #state{esql_connection=Conn}) ->
+    esql:close(Conn),
+    ok.
+
+% @doc 
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
